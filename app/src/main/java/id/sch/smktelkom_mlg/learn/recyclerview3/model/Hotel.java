897c61da7a0ba90643aaba1bf9581c0e7e6fad05
Firmaydani <firmaydaniwahyualamsyah@gmail.com>
2016-11-04 09:00:35 +0700

    RecyclerView View Detail.

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -1,7 +1,5 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3.model;
 
-import android.graphics.drawable.Drawable;
-
 /**
  * Created by Firmaydani on 11/2/2016.
  */
@@ -9,11 +7,15 @@ import android.graphics.drawable.Drawable;
 public class Hotel {
     public String judul;
     public String deskripsi;
-    public Drawable foto;
+    public String detail;
+    public String lokasi;
+    public String foto;
 
-    public Hotel(String judul, String deskripsi, Drawable foto) {
+    public Hotel(String judul, String deskripsi, String detail, String lokasi, String foto) {
         this.judul = judul;
         this.deskripsi = deskripsi;
+        this.detail = detail;
+        this.lokasi = lokasi;
         this.foto = foto;
     }
 }
